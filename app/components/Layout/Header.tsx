e88a7050612e68465986c830c2a04826f07f4ae5
kany187 <geremy.ibobi@yahoo.fr>
2026-07-08 00:25:52 -0400

    Implemented Media query for the header section. Added the logo as well

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { IoMdNotifications } from "react-icons/io";
 import { LiaUserCircleSolid } from "react-icons/lia";
+import Logo from '../../assets/logo.png'
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className='bg-base'>
-      <div className='flex items-center justify-between px-20 py-5'>
+      <div className='flex items-center justify-between px-5 py-5 md:px-20'>
         {/* Logo Section */}
-        <p>Logo</p>
+        <Image src={Logo}
+          alt='LFL Cup Logo'
+          width={50}
+          height={50}
+        />
         
         {/* Navigation Menu */}
-          <ul className='flex space-x-12'>
-              <li> ACCUEIL</li>
+          <ul className='flex space-x-12 text-sm font-light'>
+              <li className='underline'> ACCUEIL</li>
               <li>CONTACT</li>
           </ul>
     </div>
